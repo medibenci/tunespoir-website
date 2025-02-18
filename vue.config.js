5a8bfe185054e50dd93683f6043da9744a94c344
medibenci <youremail@example.com>
2025-02-18 13:20:05 +0100

    Deploy updated build files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: '/tunespoir-website/'  // Update this to your repository name
+};
+
